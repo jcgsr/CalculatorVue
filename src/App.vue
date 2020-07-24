438ea7450538057dd9f27037bdad51383bd60eda
jcgsr <cabralguerra@gmail.com>
2020-07-23 23:50:44 -0300

    final commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1 class="animate__animated animate__bounce">CALCULATOR VUE</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+export default {
+  components: { Calculator }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+*,
+html,
+body {
+  font-family: "Roboto", sans-serif;  
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1 {
+  padding: 2rem;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #00f260; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #0575e6,
+    #00f260
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #0575e6,
+    #00f260
+  );   
+}
+</style>
